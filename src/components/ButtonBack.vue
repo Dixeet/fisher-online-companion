3d91608b1031cd8e0073ebbb69cf5927112cb1da
Robin <robin.lebert@gmail.com>
2023-09-26 23:37:19 +0200

    feat: component ButtonBack

---
+++ src/components/ButtonBack.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-btn density="default" size="small" :icon="IconBack" @click="back" />
+</template>
+
+<script setup>
+import IconBack from '~icons/pajamas/go-back';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+function back() {
+  router.back();
+}
+</script>
+
+<style></style>
