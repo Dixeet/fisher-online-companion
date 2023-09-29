593efb45f1abcbba24e6f3967b67f3216d9a26af
Robin <robin.lebert@gmail.com>
2023-09-29 17:05:13 +0200

    feat: add FishListCard component

---
+++ src/components/FishListCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="v-row">
+      <div
+        v-for="(fish, index) in fishes"
+        :key="index"
+        class="v-col-12 v-col-sm-4 v-col-md-3 v-col-xl-2"
+      >
+        <slot>
+          <FishCard width="250" :fish="fish" />
+        </slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  fishes: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<style></style>
