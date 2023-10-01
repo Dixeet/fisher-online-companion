89bdfce0fea32614be82656b1c387aa66f3558b1
Robin <robin.lebert@gmail.com>
2023-10-01 15:27:48 +0200

    refactor: delete unnecessary theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -14,16 +14,6 @@ export default createVuetify({
   theme: {
     defaultTheme: 'dark',
     themes: {
-      light: {
-        colors: {
-          primary: '#1867c0',
-          secondary: '#5CBBF6',
-          tertiary: '#E57373',
-          accent: '#005CAF',
-          quarternary: '#B0D1E8',
-          'surface-bright': '#fafafa',
-        },
-      },
       dark: {
         colors: {
           primary: '#2196F3',
